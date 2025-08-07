03d2609deffac7871ff914defaf9e1a2d82cf058
Sadra Khalaj <sadra.khalaj@gmail.com>
2025-08-07 14:39:07 +0430

    try catch part finished

--- js/app.js
+++ js/app.js
@@ -1,24 +1,13 @@
-async function showContect() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await response.json();
-
-  console.log(data);
+async function showComments() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("posts has error.");
+  } finally {
+    console.log("posts uploaded:)");
+  }
 }
 
-showContect();
-
-// function test(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(() => {
-//             const user = {userName : "sadra"}
-
-//             if(user){
-//                 resolve(user)
-//             }else{
-//                 reject(new Error("user not found."))
-//             }
-//         },3000);
-//     })
-// }
-
-// test().then((user)=> user.userName).then((userName)=> console.log(userName)).catch((error)=> console.log(error.message)).finally(()=> console.log("finished"))
+showComments();
